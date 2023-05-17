ba3f9c986445cd8d7d989ad497951df330fa3c80
saswataChikpea <saswata@chikpea.com>
2023-05-17 20:41:08 +0530

    170523

--- force-app/main/default/lwc/createDCFLines/createDCFLines.js
+++ force-app/main/default/lwc/createDCFLines/createDCFLines.js
@@ -1,11 +1,10 @@
 import { LightningElement, track, wire, api } from 'lwc';
 import fetchDcf from '@salesforce/apex/dcfController.getDcf';
+import dcfLines from '@salesforce/apex/dcfController.getDcfLines';
 import DCF from '@salesforce/schema/DCF__c';
 import DCF_LINE from '@salesforce/schema/DCF_Line__c';
-import dcfLines from '@salesforce/apex/dcfController.getDcfLines';
 import { refreshApex } from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-
 
 export default class CreateDCFLines extends LightningElement {
     @track phaseDetails = true;
@@ -13,92 +12,83 @@ export default class CreateDCFLines extends LightningElement {
     @api recordId;
     @track newDCFRecord = true;
     @track showNewDcfLine;
+    @track dcfLineId;
+    @track dcfLines;
+    @track existingDCF = false;
+    @track DCFId;
+    wiredDcfLinesResult;
     @track newDCF = DCF;
     @track newDCFline = DCF_LINE
-    @track dcfLineId
-    @track dcf;
-
-    @track DCFId;
-
-    // @wire(dcfLines, { oppId: '$DCFId' })
-    // wiredDcfLines(result) {
-    //     if (result.data) {
-    //         this.dcf = result.data;
-    //         console.log('dcf  ',this.dcf );
-    //     }
-    // }
-
-    //     renderedCallback(){
-    //         dcfLines({oppId : this.recordId}).then((data)=>{
-    //             console.log('dcflines',data);
-    //             this.dcf=data
-    //             console.log("id",data[0].DCF_Lines__r.id);
-    //     })
-    // }
 
     @wire(fetchDcf, { dcfId: '$recordId' })
-    dcfRecordData({ error, data }) {
+    wiredFetchDcf({ error, data }) {
         if (data) {
-            this.record = data;
-            console.log('record', JSON.stringify(this.record));
-
+            this.record = data[0];
+            this.newDCFRecord = false;
+            this.DCFId = this.record.Id;
+            console.log('DCFId', this.DCFId);
+            console.log('record', this.record);
             this.error = undefined;
         } else if (error) {
+            console.error('Error:', error);
             this.error = error;
-            this.yearlyGrowth = undefined;
         }
     }
+
+    @wire(dcfLines, { oppId: '$recordId' })
+    wiredDcfLines(result) {
+        this.wiredDcfLinesResult = result;
+        if (result.data) {
+            this.dcfLines = result.data;
+            console.log('dcfLines-', this.dcfLines);
+            this.error = undefined;
+        } else if (result.error) {
+            console.error('Error:', result.error);
+            this.error = result.error;
+        }
+    }
+
     addNewDcfLine() {
-        console.log("add button clicked");
+        console.log('Add button clicked');
         this.showNewDcfLine = true;
     }
+
     closeModal() {
         this.showNewDcfLine = false;
     }
 
     handleSuccessDCF(event) {
-        console.log("Success");
+        console.log('Success');
         this.DCFId = event.detail.id;
         console.log('DCFId', this.DCFId);
         this.newDCFRecord = false;
-        // refreshApex(this.DCFId);
         const toastEvent = new ShowToastEvent({
             title: 'Success',
             message: 'DCF Created',
             variant: 'success'
         });
         this.dispatchEvent(toastEvent);
-        // window.location.reload()
-
+        this.refreshDcfLines();
     }
 
     handleSuccess(event) {
-        console.log("Success");
-        refreshApex(this.wiredDcfLines);
+        console.log('Success');
         const toastEvent = new ShowToastEvent({
             title: 'Success',
             message: 'DCF Line Created',
             variant: 'success'
         });
         this.dispatchEvent(toastEvent);
-        //window.location.reload()
-        dcfLines({ oppId: this.DCFId })
-            .then((result) => {
-                this.dcf = result;
-                console.log('dcflines', result);
-                console.log("id", this.dcf);
-                console.log('DCFId',this.DCFId);
-            }).catch(error => {
-                this.error = error;
+        this.refreshDcfLines();
+        const inputFields = this.template.querySelectorAll('.contactName');
+        if (inputFields) {
+            inputFields.forEach(field => {
+                field.reset();
             });
-        
-            const inputFields = this.template.querySelectorAll(
-                '.contactName'
-            );
-            if (inputFields) {
-                inputFields.forEach(field => {
-                    field.reset();
-                });
-            }
+        }
+    }
+
+    refreshDcfLines() {
+        refreshApex(this.wiredDcfLinesResult);
     }
 }
